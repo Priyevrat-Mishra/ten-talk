52bc090c30b13c47520e6d03f0c038caf0f0f73a
Priyevrat-Mishra <priyevratmishra@gmail.com>
2026-02-11 14:59:48 +0530

    minor changes

--- src/app/room/[roomId]/page.tsx
+++ src/app/room/[roomId]/page.tsx
@@ -207,7 +207,7 @@ const Page = () => {
               inputRef.current?.focus()
             }}
             disabled={!input.trim() || isPending}
-            className="bg-zinc-800 hover:bg-green-500 text-zinc-400 px-6 text-sm font-bold hover:text-zinc-200 transition-all disabled:opacity-50 disabled:cursor-not-allowed cursor-pointer"
+            className="bg-zinc-800 hover:bg-green-500 text-zinc-400 px-6 text-sm font-bold hover:text-zinc-200 transition-all disabled:opacity-50  cursor-pointer"
           >
             SEND
           </button>
